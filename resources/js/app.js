d3a7fd0930c3de58a81e3d51ce1d99f7f3ae0c7c
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-05-27 20:47:37 +0300

    feat: integrate Fancybox library
    - Added Fancybox library to app.js
    - Installed @fancyapps/ui dependency in package.json

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,11 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 import 'preline'
+import {Fancybox} from "@fancyapps/ui";
+
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
 window.Alpine = Alpine;
+window.Fancybox = Fancybox;
 
 Alpine.start();
