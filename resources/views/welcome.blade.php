e4fcdcffb8e0d64fccf0c7081c722e131cca747b
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-05-29 10:29:06 +0300

    feat: add webmanifest, favicons, and metadata for PWA support
    - Add `site.webmanifest` for manifest configuration
    - Include favicons and PWA assets
    - Update `welcome.blade.php` with meta tags and dynamic data
    - Simplify and clean up page structure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,25 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+    <link rel="manifest" href="/site.webmanifest"/>
+
+    <meta name="author" content="{{ config('app.name') }}"/>
+    <meta name="locale" content="{{ app()->getLocale() }}"/>
+
+    <meta property="og:url" content=" {{ Request::url() }}"/>
+    <meta property="og:type" content="website"/>
+    <meta
+        property="og:title"
+        content="{{ isset($title) ? $title . ' | ' . config('app.name') : config('app.name') }}"
+    />
+    <meta property="og:image" content="{{ $image ?? asset('logo.svg') }}"/>
+
+    <title>
+        {{ isset($title) ? $title . ' | ' . config('app.name') : 'Drīzumā' . ' | ' . config('app.name') }}
+    </title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -16,64 +34,9 @@
 
 </head>
 <body
-    class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-<h1 class="text-3xl font-bold underline"> Hello world! </h1>
-<div class="hs-accordion-group">
-    <div class="hs-accordion active" id="hs-basic-heading-one">
-        <button
-            class="hs-accordion-toggle hs-accordion-active:text-blue-600 py-3 inline-flex items-center gap-x-3 w-full font-semibold text-start text-gray-800 hover:text-gray-500 focus:outline-hidden focus:text-gray-500 rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:hs-accordion-active:text-blue-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400"
-            aria-expanded="true" aria-controls="hs-basic-collapse-one">
-            <svg class="hs-accordion-active:hidden block size-3.5" xmlns="http://www.w3.org/2000/svg" width="24"
-                 height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                 stroke-linecap="round" stroke-linejoin="round">
-                <path d="M5 12h14"></path>
-                <path d="M12 5v14"></path>
-            </svg>
-            <svg class="hs-accordion-active:block hidden size-3.5" xmlns="http://www.w3.org/2000/svg" width="24"
-                 height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                 stroke-linecap="round" stroke-linejoin="round">
-                <path d="M5 12h14"></path>
-            </svg>
-            Accordion #1
-        </button>
-        <div id="hs-basic-collapse-one"
-             class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300" role="region"
-             aria-labelledby="hs-basic-heading-one">
-            <p class="text-gray-800 dark:text-neutral-200">
-                <em>This is the third item's accordion body.</em> It is hidden by default, until the collapse plugin
-                adds the appropriate classes that we use to style each element. These classes control the overall
-                appearance, as well as the showing and hiding via CSS transitions.
-            </p>
-        </div>
-    </div>
-
-    <div class="hs-accordion" id="hs-basic-heading-two">
-        <button
-            class="hs-accordion-toggle hs-accordion-active:text-blue-600 py-3 inline-flex items-center gap-x-3 w-full font-semibold text-start text-gray-800 hover:text-gray-500 focus:outline-hidden focus:text-gray-500 rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:hs-accordion-active:text-blue-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400"
-            aria-expanded="false" aria-controls="hs-basic-collapse-two">
-            <svg class="hs-accordion-active:hidden block size-3.5" xmlns="http://www.w3.org/2000/svg" width="24"
-                 height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                 stroke-linecap="round" stroke-linejoin="round">
-                <path d="M5 12h14"></path>
-                <path d="M12 5v14"></path>
-            </svg>
-            <svg class="hs-accordion-active:block hidden size-3.5" xmlns="http://www.w3.org/2000/svg" width="24"
-                 height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                 stroke-linecap="round" stroke-linejoin="round">
-                <path d="M5 12h14"></path>
-            </svg>
-            Accordion #2
-        </button>
-        <div id="hs-basic-collapse-two"
-             class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300" role="region"
-             aria-labelledby="hs-basic-heading-two">
-            <p class="text-gray-800 dark:text-neutral-200">
-                <em>This is the third item's accordion body.</em> It is hidden by default, until the collapse plugin
-                adds the appropriate classes that we use to style each element. These classes control the overall
-                appearance, as well as the showing and hiding via CSS transitions.
-            </p>
-        </div>
-    </div>
+    class="bg-[#FDFDFC] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+<div>
+    <img class="w-2xl" src="{{asset('logo.svg')}}"/>
 </div>
 </body>
 </html>
