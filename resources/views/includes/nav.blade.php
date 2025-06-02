e7b39da0df208d3dd468aad3824bc36fd83aac82
KarlisEmbergs <karlisembergs@gmail.com>
2025-06-02 19:15:12 +0300

    FIX: Navigation - fix scrolling on mobile. Add language for each loop.

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,10 +1,10 @@
-<nav class="absolute w-full z-50" x-data="{ open: false }">
-    <div class="container mx-auto flex justify-between items-center px-4 py-8 relative z-50">
+<nav class="absolute w-full z-50" x-data="handleMobileMenu()" x-init="init()">
+    <div class="lg:container lg:mx-auto flex justify-between items-center px-4 py-8 relative z-50">
         <a href="/{{ app()->getLocale() }}" class="flex items-center">
             <img src="{{ asset('images/siguldas-skati-logo.png') }}" class="w-44 lg:w-48 transition-all duration-300"
                 :class="open ? 'invert' : ''" alt="Siguldas Skati Logo" />
         </a>
-        <ul class="hidden md:flex uppercase text-white">
+        <ul class="hidden lg:flex uppercase text-white">
             <x-nav.nav-link href="#">
                 @lang('Rezervēt')
             </x-nav.nav-link>
@@ -17,10 +17,17 @@
             <x-nav.nav-link href="#">
                 @lang('Kontakti')
             </x-nav.nav-link>
+
+            @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+            <x-nav.nav-link rel="alternate" hreflang="{{ $localeCode }}"
+                href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+                {{ $properties['native'] }}
+            </x-nav.nav-link>
+            @endforeach
         </ul>
 
 
-        <button x-on:click="open = !open; $dispatch('toggle-menu')" class="md:hidden relative z-50">
+        <button x-on:click="open = !open" class="lg:hidden relative z-50">
             <svg x-show="!open" class="size-12" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 20 20">
                 <path fill-rule="evenodd"
                     d="M2 4.75A.75.75 0 0 1 2.75 4h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 4.75ZM2 10a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 10Zm0 5.25a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Z"
@@ -34,11 +41,10 @@
         </button>
     </div>
 
-    <div x-data="{ open: false }" x-on:toggle-menu.window="open = !open" x-show="open"
-        x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0 translate-x-full"
-        x-transition:enter-end="opacity-100 translate-x-0" x-transition:leave="transition ease-in duration-300"
-        x-transition:leave-start="opacity-100 translate-x-0" x-transition:leave-end="opacity-0 translate-x-full"
-        class="fixed inset-0 bg-bg bg-opacity-90 z-40 md:hidden">
+    <div x-show="open" x-transition:enter="transition ease-out duration-300"
+        x-transition:enter-start="opacity-0 translate-x-full" x-transition:enter-end="opacity-100 translate-x-0"
+        x-transition:leave="transition ease-in duration-300" x-transition:leave-start="opacity-100 translate-x-0"
+        x-transition:leave-end="opacity-0 translate-x-full" class="fixed inset-0 bg-bg bg-opacity-90 z-40 md:hidden">
 
         <ul class="flex flex-col items-center justify-center h-full space-y-2 uppercase text-black md:text-white">
             <x-nav.nav-link href="#">
@@ -53,6 +59,14 @@
             <x-nav.nav-link href="#">
                 @lang('Kontakti')
             </x-nav.nav-link>
+            <ul class="flex"> @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                <x-nav.nav-link rel="alternate" hreflang="{{ $localeCode }}"
+                    href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+                    {{ $properties['native'] }}
+                </x-nav.nav-link>
+                @endforeach
+            </ul>
+
         </ul>
         <ul class="absolute bottom-12 left-0 right-0 flex justify-center items-center space-x-8">
             <x-social.icon href="https://www.facebook.com/ModernHouseLV">
