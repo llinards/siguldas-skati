a45e2d51db12b8a7ef9487ba000f1587e7d3b28c
KarlisEmbergs <karlisembergs@gmail.com>
2025-06-04 18:42:57 +0300

    FIX: Global - focus outline color.

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -23,56 +23,60 @@
         </div>
     </div>
 </div>
-<div class="bg-ss-dark pb-6 pt-6 sm:pt-28 lg:pt-40 lg:pb-24">
-    <div
-        class="px-4 container mx-auto flex flex-col sm:grid sm:grid-cols-2 sm:grid-rows-3 md:grid-cols-4 md:grid-rows-2 lg:flex lg:flex-row text-white justify-between">
-        <div class="flex flex-col sm:col-span-2 md:col-span-4 lg:col-span-1 md:items-center py-6 md:py-12 lg:py-0">
-            <a href="/{{ app()->getLocale() }}" class="flex items-center">
-                <img src="{{ asset('images/siguldas-skati-logo.png') }}" class="w-xs mb-4" alt="Siguldas Skati Logo" />
-            </a>
-            <p class="text-sm">@lang('© Siguldas skati 2025 | Visas tiesības rezervētas.')</p>
-        </div>
-        <div class="mb-6 lg:my-0">
-            <ul>
-                <h4 class="text-2xl font-medium mb-4">@lang('Izvēlne')</h4>
-                <x-nav.footer-link>@lang('Dizaina mājas')</x-nav.footer-link>
-                <x-nav.footer-link>@lang('BUJ')</x-nav.footer-link>
-                <x-nav.footer-link>@lang('Kontakti')</x-nav.footer-link>
-            </ul>
-        </div>
-        <div class="mb-6 lg:my-0">
-            <ul>
-                <h4 class="text-2xl md:text-xl xl:text-2xl font-medium mb-4">@lang('Informācija')</h4>
-                <x-nav.footer-link>@lang('Privātuma politika')</x-nav.footer-link>
-            </ul>
-        </div>
-        <div class="mb-6 lg:my-0">
-            <ul>
-                <h4 class="text-2xl md:text-xl xl:text-2xl font-medium mb-4">@lang('Kontakti')</h4>
-                <x-nav.footer-link href="tel:+37125666622">+371 25666622</x-nav.footer-link>
-                <x-nav.footer-link href="mailto:info@siguldasskati.lv">info@siguldasskati.lv
-                </x-nav.footer-link>
-                <x-nav.footer-link href="https://maps.app.goo.gl/gEtDt5FS3qpzUtWE9" target="_blank">@lang('Cēsu
-                    iela 17,
-                    Sigulda, Latvija')</x-nav.footer-link>
-            </ul>
-        </div>
-        <div class="mb-6 lg:my-0">
-            <h4 class="text-2xl md:text-xl xl:text-2xl font-bold mb-4">@lang('Pieseko')</h4>
-            <ul class="flex flex-row gap-4">
-                <x-social.icon href="https://www.facebook.com/ModernHouseLV">
-                    <x-social.facebook class="hover:text-gray-500 transition-colors duration-200" />
-                </x-social.icon>
+<footer>
 
-                <x-social.icon href="https://www.instagram.com/siguldasskati">
-                    <x-social.instagram class="hover:text-gray-500 transition-colors duration-200" />
-                </x-social.icon>
+    <div class="bg-ss-dark pb-6 pt-6 sm:pt-28 lg:pt-40 lg:pb-24">
+        <div
+            class="px-4 container mx-auto flex flex-col sm:grid sm:grid-cols-2 sm:grid-rows-3 md:grid-cols-4 md:grid-rows-2 lg:flex lg:flex-row text-white justify-between">
+            <div class="flex flex-col sm:col-span-2 md:col-span-4 lg:col-span-1 md:items-center py-6 md:py-12 lg:py-0">
+                <a href="/{{ app()->getLocale() }}" class="flex items-center">
+                    <img src="{{ asset('images/siguldas-skati-logo.png') }}" class="w-xs mb-4"
+                        alt="Siguldas Skati Logo" />
+                </a>
+                <p class="text-sm">@lang('© Siguldas skati 2025 | Visas tiesības rezervētas.')</p>
+            </div>
+            <div class="mb-6 lg:my-0">
+                <ul>
+                    <h4 class="text-2xl font-medium mb-4">@lang('Izvēlne')</h4>
+                    <x-nav.footer-link href="#">@lang('Dizaina mājas')</x-nav.footer-link>
+                    <x-nav.footer-link href="#">@lang('BUJ')</x-nav.footer-link>
+                    <x-nav.footer-link href="#">@lang('Kontakti')</x-nav.footer-link>
+                </ul>
+            </div>
+            <div class="mb-6 lg:my-0">
+                <ul>
+                    <h4 class="text-2xl md:text-xl xl:text-2xl font-medium mb-4">@lang('Informācija')</h4>
+                    <x-nav.footer-link href="#">@lang('Privātuma politika')</x-nav.footer-link>
+                </ul>
+            </div>
+            <div class="mb-6 lg:my-0">
+                <ul>
+                    <h4 class="text-2xl md:text-xl xl:text-2xl font-medium mb-4">@lang('Kontakti')</h4>
+                    <x-nav.footer-link href="tel:+37125666622">+371 25666622</x-nav.footer-link>
+                    <x-nav.footer-link href="mailto:info@siguldasskati.lv">info@siguldasskati.lv
+                    </x-nav.footer-link>
+                    <x-nav.footer-link href="https://maps.app.goo.gl/gEtDt5FS3qpzUtWE9" target="_blank">@lang('Cēsu
+                        iela 17,
+                        Sigulda, Latvija')</x-nav.footer-link>
+                </ul>
+            </div>
+            <div class="mb-6 lg:my-0">
+                <h4 class="text-2xl md:text-xl xl:text-2xl font-bold mb-4">@lang('Pieseko')</h4>
+                <ul class="flex flex-row gap-4">
+                    <x-social.icon href="https://www.facebook.com/ModernHouseLV">
+                        <x-social.facebook class="hover:text-gray-500 transition-colors duration-200" />
+                    </x-social.icon>
 
-                <x-social.icon href="https://www.tiktok.com/@modernhouse_lv">
-                    <x-social.tiktok class="hover:text-gray-500 transition-colors duration-200" />
-                </x-social.icon>
-            </ul>
+                    <x-social.icon href="https://www.instagram.com/siguldasskati">
+                        <x-social.instagram class="hover:text-gray-500 transition-colors duration-200" />
+                    </x-social.icon>
 
+                    <x-social.icon href="https://www.tiktok.com/@modernhouse_lv">
+                        <x-social.tiktok class="hover:text-gray-500 transition-colors duration-200" />
+                    </x-social.icon>
+                </ul>
+
+            </div>
         </div>
     </div>
-</div>
+</footer>
