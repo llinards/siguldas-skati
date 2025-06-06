3f640359700f1743ae12e18fa15efaa8f331e072
KarlisEmbergs <karlisembergs@gmail.com>
2025-06-07 01:15:34 +0300

    Hover effect improvement for the gallery carousel.

--- resources/views/components/carousels/gallery/card.blade.php
+++ resources/views/components/carousels/gallery/card.blade.php
@@ -1,6 +1,9 @@
 <figure class="f-carousel__slide relative">
     <a id="gallery-main-{{ $imageGallery }}" href="{{ $imageLink ?? '' }}" @if(!empty($extraImages))
         data-gallery-extra='{{ $extraImages }}' @endif>
-        <img class="rounded-3xl object-cover w-full min-h-136 transition-transform duration-300 ease-in-out hover:scale-105" alt="" src="{{ $imageLink ?? '' }}" />
+        <div class="group relative rounded-3xl overflow-hidden">
+            <img class="rounded-3xl object-cover w-full min-h-136 transition-all duration-300 ease-in-out group-hover:scale-105 group-hover:rounded-2xl"
+                alt="" src="{{ $imageLink ?? '' }}" />
+        </div>
     </a>
 </figure>
