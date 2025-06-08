2789f45ea4b254b3e7546a86402a81036f80f767
KarlisEmbergs <karlisembergs@gmail.com>
2025-06-08 11:51:52 +0300

    FIX: Navigation - additional styling fixes based on the page.

--- resources/views/components/nav/nav-link.blade.php
+++ resources/views/components/nav/nav-link.blade.php
@@ -1,11 +1,10 @@
 <li>
     @props(['active' => false])
-    <a {{$attributes->merge(['class' => ($active ? ' border-b-1' : '' ) . 'mx-4 border-1 text-center border-transparent
-        hover:border-b-menu-hover
-        hover:text-menu-hover
-        lg:hover:border-b-white lg:hover:text-white transition-all ease-in-out duration-200 text-btn-md md:text-btn-md
-        lg:text-btn']) }}
-        >
+    <a {{ $attributes->class([
+        'mx-4 border-1 text-center border-transparent hover:border-b-menu-hover hover:text-menu-hover transition-all ease-in-out duration-200 text-btn-md md:text-btn-md lg:text-btn',
+        'border-b-1' => $active,
+        'lg:hover:border-b-white lg:hover:text-white' => Route::is('home')
+    ]) }}>
         {{ $slot }}
     </a>
 </li>
