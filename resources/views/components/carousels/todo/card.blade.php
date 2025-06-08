4f29e10e878abb92e73b3fb72e48400cfdd3a318
KarlisEmbergs <karlisembergs@gmail.com>
2025-06-08 11:12:56 +0300

    FIX: Product cards and Todo - slight spacing and alignment adjustment.

--- resources/views/components/carousels/todo/card.blade.php
+++ resources/views/components/carousels/todo/card.blade.php
@@ -6,8 +6,8 @@
             <div class="absolute inset-0 rounded-3xl pointer-events-none bg-gradient-to-t from-black/30 via-black/20 to-transparent transition-transform duration-300 ease-in-out group-hover:scale-105"></div>
         </div>
     </a>
-    <div class="absolute w-4/6 left-1/2 bottom-12 -translate-x-1/2 z-10 text-white text-center">
-        <h4 class="mb-6 text-h-sm-mob sm:text-2xl md:text-3xl xl:text-4xl">{{ $todoTitle ?? '' }}</h4>
+    <div class="absolute w-full max-w-[320px] left-1/2 bottom-12 -translate-x-1/2 z-10 text-white text-center flex flex-col items-center">
+        <h4 class="mb-3 text-h-sm-mob sm:text-2xl md:text-3xl xl:text-4xl">{{ $todoTitle ?? '' }}</h4>
         <x-btn-secondary href="{{ $todoLink ?? '' }}">
             @lang('Uzzināt vairāk')
         </x-btn-secondary>
