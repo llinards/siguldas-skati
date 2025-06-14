9297de4d7370e062000bf4a4cb11447ae557a2f3
KarlisEmbergs <karlisembergs@gmail.com>
2025-06-14 15:02:51 +0300

    Add active link styling.

--- resources/views/components/nav/nav-link.blade.php
+++ resources/views/components/nav/nav-link.blade.php
@@ -1,10 +1,10 @@
 <li>
     @props(['active' => false])
     <a {{ $attributes->class([
-        'mx-4 border-1 text-center border-transparent hover:border-b-menu-hover hover:text-menu-hover transition-all ease-in-out duration-200 text-btn-md md:text-btn-md lg:text-btn',
-        'border-b-1' => $active,
+        'mx-4 border-1 text-center border-transparent hover:border-b-menu-hover hover:text-menu-hover transition-all
+        ease-in-out duration-200 text-btn-md md:text-btn-md lg:text-btn',
         'lg:hover:border-b-white lg:hover:text-white' => Route::is('home')
-    ]) }}>
+        ]) }}>
         {{ $slot }}
     </a>
 </li>
