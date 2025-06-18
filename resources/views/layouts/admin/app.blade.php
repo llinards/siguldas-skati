e64c53adc3c7fca56b390678e45064700577bb18
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-06-18 08:33:04 +0300

    refactor(views): remove unused components and update layouts
    - Delete unused `admin/app-layout` component.
    - Simplify and clean up Blade templates for improved maintainability.
    - Update favicon and manifest links in admin layout.
    - Adjust styles and DOM properties for layout consistency.
    - Comment out unused navigation links in `admin-nav`.

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+    <link rel="manifest" href="/site.webmanifest"/>
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
@@ -16,7 +21,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="antialiased bg-bg font-main">
+<body class="antialiased bg-ss font-main">
 <div class="min-h-screen">
     @include('includes.admin-nav')
 
