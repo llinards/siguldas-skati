7b5fc231a1ce109956839c96a1e2cd6967242cb0
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-06-25 11:05:59 +0300

    feat(views): add favicons and update guest layout styles
    - Add favicon links for multiple sizes and manifest.
    - Update body class in `guest.blade.php` for styling.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+    <link rel="manifest" href="/site.webmanifest"/>
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
@@ -14,7 +19,7 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="font-sans text-gray-900 antialiased">
+<body class="font-main">
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-ss">
     <div class="mb-6">
         <x-application-logo class="w-20 h-20 fill-current text-gray-500"/>
