0718aa21c6bf670ed0df918bc9a68e5da5134f36
KarlisEmbergs <karlisembergs@gmail.com>
2025-07-20 14:00:50 +0300

    FIX: Modal background scrollability. Testing if using the built in modal fixes the issue.

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,21 +1,20 @@
 @props([
-    'name',
-    'show' => false,
-    'maxWidth' => '2xl',
+'name',
+'show' => false,
+'maxWidth' => '2xl',
 ])
 
 @php
-    $maxWidth = [
-        'sm' => 'sm:max-w-sm',
-        'md' => 'sm:max-w-md',
-        'lg' => 'sm:max-w-lg',
-        'xl' => 'sm:max-w-xl',
-        '2xl' => 'sm:max-w-2xl',
-    ][$maxWidth];
+$maxWidth = [
+'sm' => 'sm:max-w-sm',
+'md' => 'sm:max-w-md',
+'lg' => 'sm:max-w-lg',
+'xl' => 'sm:max-w-xl',
+'2xl' => 'sm:max-w-2xl',
+][$maxWidth];
 @endphp
 
-<div
-    x-data="{
+<div x-data="{
         show: @js($show),
         focusables() {
             // All focusable element types...
@@ -30,8 +29,7 @@
         prevFocusable() { return this.focusables()[this.prevFocusableIndex()] || this.lastFocusable() },
         nextFocusableIndex() { return (this.focusables().indexOf(document.activeElement) + 1) % (this.focusables().length + 1) },
         prevFocusableIndex() { return Math.max(0, this.focusables().indexOf(document.activeElement)) -1 },
-    }"
-    x-init="
+    }" x-init="
         $watch('show', (value) => {
             if (value) {
                 document.body.classList.add('overflow-y-hidden')
@@ -40,41 +38,25 @@
                 document.body.classList.remove('overflow-y-hidden')
             }
         })
-    "
-    x-on:open-modal.window="$event.detail == '{{ $name }}' ? (show = true) : null"
-    x-on:close-modal.window="$event.detail == '{{ $name }}' ? (show = false) : null"
-    x-on:close.stop="show = false"
-    x-on:keydown.escape.window="show = false"
-    x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
-    x-on:keydown.shift.tab.prevent="prevFocusable().focus()"
-    x-show="show"
-    class="fixed inset-0 z-50 overflow-y-auto px-4 py-6 sm:px-0"
-    style="display: {{ $show ? 'block' : 'none' }}"
->
-    <div
-        x-show="show"
-        class="fixed inset-0 transform transition-all"
-        x-on:click="show = false"
-        x-transition:enter="duration-300 ease-out"
-        x-transition:enter-start="opacity-0"
-        x-transition:enter-end="opacity-100"
-        x-transition:leave="duration-200 ease-in"
-        x-transition:leave-start="opacity-100"
-        x-transition:leave-end="opacity-0"
-    >
-        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+    " x-on:open-modal.window="$event.detail == '{{ $name }}' ? (show = true) : null"
+    x-on:close-modal.window="$event.detail == '{{ $name }}' ? (show = false) : null" x-on:close.stop="show = false"
+    x-on:keydown.escape.window="show = false" x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
+    x-on:keydown.shift.tab.prevent="prevFocusable().focus()" x-show="show"
+    class="fixed inset-0 z-50 flex items-center justify-center" style="display: {{ $show ? 'block' : 'none' }}">
+    <div x-show="show" class="fixed inset-0 transform transition-all" x-on:click="show = false"
+        x-transition:enter="duration-300 ease-out" x-transition:enter-start="opacity-0"
+        x-transition:enter-end="opacity-100" x-transition:leave="duration-200 ease-in"
+        x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
+        <div class="absolute inset-0 bg-black opacity-75"></div>
     </div>
 
-    <div
-        x-show="show"
-        class="{{ $maxWidth }} mb-6 transform overflow-hidden rounded-lg bg-white shadow-xl transition-all sm:mx-auto sm:w-full"
+    <div x-show="show"
+        class="{{ $maxWidth }} fixed max-h-[90vh] transform flex flex-col overflow-y-auto rounded-xl bg-ss border shadow-2xs transition-all w-xs sm:w-lg md:w-xl xl:w-2xl relative z-10"
         x-transition:enter="duration-300 ease-out"
         x-transition:enter-start="translate-y-4 opacity-0 sm:translate-y-0 sm:scale-95"
-        x-transition:enter-end="translate-y-0 opacity-100 sm:scale-100"
-        x-transition:leave="duration-200 ease-in"
+        x-transition:enter-end="translate-y-0 opacity-100 sm:scale-100" x-transition:leave="duration-200 ease-in"
         x-transition:leave-start="translate-y-0 opacity-100 sm:scale-100"
-        x-transition:leave-end="translate-y-4 opacity-0 sm:translate-y-0 sm:scale-95"
-    >
+        x-transition:leave-end="translate-y-4 opacity-0 sm:translate-y-0 sm:scale-95">
         {{ $slot }}
     </div>
 </div>
