df9c79edbf76d908708b163acae1d913891a179c
KarlisEmbergs <karlisembergs@gmail.com>
2025-07-20 15:37:20 +0300

    Modal. Adjust styling.

--- resources/views/components/main-modal.blade.php
+++ resources/views/components/main-modal.blade.php
@@ -1,8 +1,8 @@
 <div id="{{ $modalId }}"
-    class="hs-overlay hidden size-full fixed top-0 start-0 z-80 overflow-x-hidden overflow-y-auto pointer-events-none"
+    class="hs-overlay hidden size-full fixed top-0 start-0 z-80 overflow-x-hidden overflow-y-auto pointer-events-none items-center justify-center"
     role="dialog" tabindex="-1" aria-labelledby="modal-label">
     <div
-        class="hs-overlay-animation-target hs-overlay-open:scale-100 hs-overlay-open:opacity-100 scale-95 opacity-0 ease-in-out transition-all duration-200 sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-56px)] flex items-center">
+        class="hs-overlay-animation-target hs-overlay-open:scale-100 hs-overlay-open:opacity-100 scale-95 opacity-0 ease-in-out transition-all duration-200 flex justify-center items-center p-4 min-h-screen sm:max-w-lg sm:w-full sm:mx-auto">
         <div
             class="w-full flex flex-col bg-white max-h-[90vh] border border-ss-dark shadow-2xs rounded-xl pointer-events-auto">
             <div class="flex justify-between items-center py-3 px-4 border-b border-ss-dark">
