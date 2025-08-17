2a9e449205219bd20efc1abc686bea42cb259462
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-08-17 21:20:22 +0300

    refactor(blade): adjust alignment and spacing in home view
    - Update container to use `justify-end` class for better alignment.
    - Refactor header and button spacing with `mb-*` classes.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,14 +16,14 @@
         </div>
 
         <!-- Foreground content -->
-        <div class="container relative z-10 mx-auto flex flex-col items-center px-4">
+        <div class="container relative z-10 mx-auto flex flex-col justify-end items-center px-4">
             <h1
-                class="text-h-mob xs:text-6xl font-heading absolute top-48 max-w-7xl text-center leading-12 text-white uppercase sm:top-1/2 sm:-translate-y-1/2 xs:leading-16 md:text-6xl xl:leading-28">
+                class="mb-6 text-h-mob xs:text-6xl font-heading max-w-7xl text-center leading-12 text-white uppercase xs:leading-16 md:text-6xl xl:leading-28">
                 @lang('Modernas brīvdienu dizaina mājas tavai atpūtai!')
             </h1>
 
             <x-btn-header target="_blank" href="https://www.booking.com/hotel/lv/siguldas-skati-sigulda.lv.html"
-                          class="absolute bottom-48 sm:bottom-16">
+                          class="mb-24">
                 @lang('Rezervēt')
             </x-btn-header>
         </div>
