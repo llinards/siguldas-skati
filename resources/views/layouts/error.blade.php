3e6a6dd48ed0bf9d4beaca1157fee72d5b8cba5d
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-08-18 22:57:13 +0300

    refactor(blade): improve indentation and remove redundant classes in `error.blade.php`

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+    <link rel="manifest" href="/site.webmanifest"/>
 
-        <title>{{ isset($title) ? $title . ' | ' . config('app.name') : config('app.name') }}</title>
+    <title>{{ isset($title) ? $title . ' | ' . config('app.name') : config('app.name') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-    <body class="font-main">
-        <div class="bg-ss flex min-h-screen flex-col items-center pt-6 sm:justify-center sm:pt-0">
-            <div class="mb-2">
-                <x-application-logo class="h-20 w-20 fill-current text-gray-500" />
-            </div>
+<body class="font-main">
+<div class="bg-ss flex min-h-screen flex-col items-center pt-6 justify-center">
+    <div class="mb-2">
+        <x-application-logo class="h-20 w-20 fill-current text-gray-500"/>
+    </div>
 
-            <div class="w-full overflow-hidden px-6 py-4 sm:max-w-md sm:rounded-lg">
-                {{ $slot }}
-            </div>
-        </div>
-    </body>
+    <div class="w-full overflow-hidden px-6 py-4 max-w-md rounded-lg">
+        {{ $slot }}
+    </div>
+</div>
+</body>
 </html>
