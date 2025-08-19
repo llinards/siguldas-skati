70c3322483fe2d05c173fa49af30bc2305e1bb0b
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-08-19 16:38:34 +0300

    - **fix(products, btn-back): adjust layout and styles for improved alignment**   - Update product heading and button alignment for better visual structure.   - Refine styles in `btn-back` component for consistent theme and improved hover effects.

--- resources/views/components/btn-back.blade.php
+++ resources/views/components/btn-back.blade.php
@@ -1,13 +1,13 @@
-<a href="{{ route('home') }}" id="backBtn" aria-label="Previous page" 
+<a href="{{ route('home') }}" id="backBtn" aria-label="Previous page"
     {{ $attributes->merge([
-        'class' => 'inline-block px-3 py-2 mb-3 lg:mb-6 uppercase bg-ss-dark rounded-xl text-white text-center
-        hover:bg-white
-        hover:text-black cursor-pointer hover:border-ss-dark border-1 border-transparent transition-all duration-300'
+        'class' => 'inline-block px-3 py-2 mb-3 lg:mb-6 uppercase bg-ss rounded-xl text-black text-center
+        hover:bg-ss-dark
+        hover:text-white cursor-pointer hover:border-transparent border-1 border-ss-dark transition-all duration-300'
     ]) }}>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-7">
         <path fill-rule="evenodd"
-            d="M9.78 4.22a.75.75 0 0 1 0 1.06L7.06 8l2.72 2.72a.75.75 0 1 1-1.06 1.06L5.47 8.53a.75.75 0 0 1 0-1.06l3.25-3.25a.75.75 0 0 1 1.06 0Z"
-            clip-rule="evenodd" />
+              d="M9.78 4.22a.75.75 0 0 1 0 1.06L7.06 8l2.72 2.72a.75.75 0 1 1-1.06 1.06L5.47 8.53a.75.75 0 0 1 0-1.06l3.25-3.25a.75.75 0 0 1 1.06 0Z"
+              clip-rule="evenodd"/>
     </svg>
 </a>
 
@@ -15,7 +15,7 @@
     const backBtn = document.getElementById('backBtn');
 
     backBtn.addEventListener('click', function (event) {
-        event.preventDefault(); 
+        event.preventDefault();
         const prev = document.referrer;
         const homeUrl = "{{ route('home') }}";
 
