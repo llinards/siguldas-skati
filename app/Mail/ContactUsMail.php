929054536c93d219899e39eb5c932d6096a100b5
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-08-21 18:54:27 +0300

    Update ContactUsMail.php

--- app/Mail/ContactUsMail.php
+++ app/Mail/ContactUsMail.php
@@ -24,7 +24,6 @@ class ContactUsMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            //            to: 'info@siguldasskati.lv',
             to: 'siguldasskati@gmail.com',
             replyTo: $this->email,
             subject: 'Jauns kontaktu ziņojums no mājas lapas',
